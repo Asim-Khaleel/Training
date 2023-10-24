c244c5a05afdf2c172487fe1e5dfa0a2339af369
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-10-24 22:07:01 +0500

    Implemented Client side validation

---
+++ Task1/Portfolio.js
@@ -0,0 +1,6 @@
+const linkToSignupPage = "../Task2/Task2.html";
+
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", function() {
+  window.location.href = linkToSignupPage;
+});
