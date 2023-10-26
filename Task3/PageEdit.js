b808622fb07b456c1e1f8dbac06c3f0068d09e3b
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-10-26 13:10:02 +0500

    Task3 in progress

---
+++ Task3/PageEdit.js
@@ -0,0 +1,6 @@
+const linkToPortfolioPage = "../Task1/Task1.html";
+
+const goBackButton = document.getElementById("goBackButton");
+goBackButton.addEventListener("click", function () {
+    window.location.href = linkToPortfolioPage;
+})
