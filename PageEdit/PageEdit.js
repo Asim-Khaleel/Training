e258a611fc5e2b96ea038a5025f61898db540168
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-10-31 14:30:49 +0500

    Completed the task

--- PageEdit/PageEdit.js
+++ PageEdit/PageEdit.js
@@ -4,33 +4,57 @@ function redirectToPortfolioPage() {
   window.location.href = LinkToPortfolioPage;
 }
 
-  const addExperienceButton = document.getElementById("addExperienceButton");
-  const experienceForm = document.getElementById("experienceForm");
-  const saveExperienceButton = document.getElementById("saveExperienceButton");
-  const experienceEntries = document.getElementById("experienceEntries");
+const addExperienceButton = document.getElementById("addExperienceButton");
+const experienceForm = document.getElementById("experienceForm");
+const saveExperienceButton = document.getElementById("saveExperienceButton");
+const experienceEntries = document.getElementById("experienceEntries");
 
-  addExperienceButton.addEventListener("click", () => {
-    experienceForm.style.display = "flex";
-  });
 
-  saveExperienceButton.addEventListener("click", () => {
-    const companyName = document.getElementById("companyName").value;
-    const startDate = document.getElementById("startDate").value;
-    const endDate = document.getElementById("endDate").value;
-    const description = document.getElementById("description").value;
+let experienceData = JSON.parse(localStorage.getItem("experienceData")) || [];
 
+function saveToLocalStorage() {
+  localStorage.setItem("experienceData", JSON.stringify(experienceData));
+}
+
+function displayExperienceData() {
+  experienceEntries.innerHTML = "";
+  experienceData.forEach((experience) => {
     const experienceEntry = document.createElement("div");
     experienceEntry.classList.add("experience-entry");
     experienceEntry.style.margin = "10px 0px";
     experienceEntry.innerHTML = `
-      <div><strong> ${companyName}</strong></div>
-
-      <div><strong>${startDate} - ${endDate}</strong> </div>
-      <div> ${description}</div>
+      <div><strong>${experience.companyName}</strong></div>
+      <div><strong>${experience.startDate} - ${experience.endDate}</strong></div>
+      <div>${experience.description}</div>
     `;
     experienceEntries.appendChild(experienceEntry);
-
-    experienceForm.reset();
-    experienceForm.style.display = "none";
   });
+}
 
+addExperienceButton.addEventListener("click", () => {
+  experienceForm.style.display = "flex";
+});
+
+saveExperienceButton.addEventListener("click", () => {
+  const companyName = document.getElementById("companyName").value;
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  const description = document.getElementById("description").value;
+
+  const experienceEntry = {
+    companyName,
+    startDate,
+    endDate,
+    description,
+  };
+
+  experienceData.push(experienceEntry);
+  saveToLocalStorage();
+
+  displayExperienceData();
+
+  experienceForm.reset();
+  experienceForm.style.display = "none";
+});
+
+displayExperienceData();
--- Portfolio/Portfolio.js
+++ Portfolio/Portfolio.js
@@ -8,3 +8,25 @@ function redirectToSignupPage() {
 function redirectToEditPage() {
   window.location.href = LinkToEditPage;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Retrieve experience data from local storage
+  const experienceData = JSON.parse(localStorage.getItem("experienceData"));
+
+  // Check if there's data and display it
+  if (experienceData && experienceData.length > 0) {
+    const experienceElement = document.querySelector(".experience");
+
+    // Create and append new experience entries
+    experienceData.map((experience) => {
+      const experienceEntry = document.createElement("div");
+      experienceEntry.style.margin = "10px";
+      experienceEntry.innerHTML = `
+      <div><strong>${experience.companyName}</strong></div>
+      <div><strong>${experience.startDate} - ${experience.endDate}</strong></div>
+      <div>${experience.description}</div>
+    `;
+      experienceElement.appendChild(experienceEntry);
+    });
+  }
+});
