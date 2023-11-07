d6fba672f138efe5c7dddea68b6a54c62ed4d00c
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-11-07 14:57:44 +0500

    started working on Task 5

--- PageEdit/PageEdit.js
+++ PageEdit/PageEdit.js
@@ -16,22 +16,84 @@ function saveToLocalStorage() {
 }
 
 function displayExperienceData() {
-  experienceData.map((experience) => {
+  experienceEntries.innerHTML = '';
+  experienceData.forEach((experience, index) => {
     const experienceEntry = document.createElement("div");
     experienceEntry.innerHTML = `
       <div><strong>${experience.companyName}</strong></div>
       <div><strong>${experience.startDate} - ${experience.endDate}</strong></div>
       <div>${experience.description}</div>
+      <div>
+        <button class="delete-button" data-index="${index}">Delete</button>
+        <button class="edit-button" data-index="${index}">Edit</button>
+      </div>
     `;
     experienceEntries.appendChild(experienceEntry);
+
+    const deleteButton = experienceEntry.querySelector(".delete-button");
+    deleteButton.addEventListener("click", () => deleteExperience(index));
+
+    const editButton = experienceEntry.querySelector(".edit-button");
+    editButton.addEventListener("click", () => showEditForm(index));
   });
 }
 
+function deleteExperience(index) {
+  experienceData.splice(index, 1);
+  saveToLocalStorage();
+  displayExperienceData();
+}
+
+function showEditForm(index) {
+  const experienceEntry = experienceData[index];
+  document.getElementById("companyName").value = experienceEntry.companyName;
+  document.getElementById("startDate").value = experienceEntry.startDate;
+  document.getElementById("endDate").value = experienceEntry.endDate;
+  document.getElementById("description").value = experienceEntry.description;
+
+  saveExperienceButton.removeEventListener("click", saveNewExperience);
+  saveExperienceButton.addEventListener("click", () => saveEditedExperience(index));
+
+  experienceForm.style.display = "flex";
+}
+
+function saveEditedExperience(index) {
+  const companyName = document.getElementById("companyName").value;
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  const description = document.getElementById("description").value;
+
+  if (
+    companyName === "" ||
+    startDate === "" ||
+    endDate === "" ||
+    description === ""
+  ) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  const experienceEntry = {
+    companyName,
+    startDate,
+    endDate,
+    description,
+  };
+
+  experienceData[index] = experienceEntry;
+  saveToLocalStorage();
+  experienceForm.style.display = "none";
+  displayExperienceData();
+}
+
 addExperienceButton.addEventListener("click", () => {
+  experienceForm.reset();
+  saveExperienceButton.removeEventListener("click", saveEditedExperience);
+  saveExperienceButton.addEventListener("click", saveNewExperience);
   experienceForm.style.display = "flex";
 });
 
-saveExperienceButton.addEventListener("click", () => {
+function saveNewExperience() {
   const companyName = document.getElementById("companyName").value;
   const startDate = document.getElementById("startDate").value;
   const endDate = document.getElementById("endDate").value;
@@ -59,6 +121,8 @@ saveExperienceButton.addEventListener("click", () => {
 
   experienceForm.reset();
   experienceForm.style.display = "none";
-});
+  displayExperienceData();
+}
 
 displayExperienceData();
+
