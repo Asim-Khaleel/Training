ad540f305ad1e0f40dc3dc7a6bf51b7ada9f6891
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-11-10 11:13:10 +0500

    Completed Task for Api Integration

--- Signup/Signup.js
+++ Signup/Signup.js
@@ -3,12 +3,34 @@ const nameErrorMessage = "Name is required.";
 const emailErrorMessage = "Email is required and must contain '@'.";
 const passwordErrorMessage = "Password must have at least 8 characters.";
 const registrationSuccessMessage = "Registration successful \u{1F603}";
+const apiEndpoint = 'https://dummyjson.com/docs/users/#add';
 
 function redirectToPortfolioPage() {
   window.location.href = LINK_TO_PORTFOLIOPAGE;
 }
 
-function validateForm() {
+async function sendFormDataToAPI(name, email, password) {
+  try {
+    const response = await fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    if (response.ok) {
+      return { success: true };
+    } else {
+      const data = await response.json();
+      return { success: false, error: data.message };
+    }
+  } catch (error) {
+    return { success: false, error: 'An error occurred while processing your request.' };
+  }
+}
+
+async function validateForm() {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -49,7 +71,14 @@ function validateForm() {
   }
 
   if (isValid) {
-    successMessage.textContent = registrationSuccessMessage;
-    successMessage.style.color = "green";
+    const apiResponse = await sendFormDataToAPI(name, email, password);
+
+    if (apiResponse.success) {
+      successMessage.textContent = registrationSuccessMessage;
+      successMessage.style.color = "green";
+    } else {
+      successMessage.textContent = `Error: ${apiResponse.error}`;
+      successMessage.style.color = "red";
+    }
   }
 }
