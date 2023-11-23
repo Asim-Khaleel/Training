f239b49b4b5f847224a0856886e606e3697b5219
Asim Khalil <asim.khalil@codeninjaconsulting.com>
2023-11-23 17:41:46 +0500

    Task9: Working on setting up the backend

--- database_Storage/controllers/experienceController.js
+++ database_Storage/controllers/experienceController.js
@@ -1,18 +1,31 @@
-const getExperience = (req, res) => {
-    res.json({message : " Get Experience"})
-}
+const asyncHandler = require('express-async-handler')
 
-const setExperience = (req, res) => {
+const Experience = require('../model/experienceModel')
+
+
+const getExperience =asyncHandler(async (req, res) => {
+
+    const experience = await Experience.find()
+    res.json(experience)
+})
+
+
+const setExperience = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+      }
+      const experience = await Experience.create()
     res.json({message : " Set Experience"})
-}
+})
 
-const updateExperience = (req, res) => {
+const updateExperience = asyncHandler(async (req, res) => {
     res.json({message : `Update Experience at ${req.params.id}`})
-}
+})
 
-const deleteExperience = (req, res) => {
+const deleteExperience = asyncHandler(async (req, res) => {
     res.json({message : `Delete Experience at ${req.params.id}`})
-}
+})
 
 module.exports = {
 getExperience,
---
+++ database_Storage/model/experienceModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const experienceSchema = mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please add company name'],
+    },
+    startDate: {
+      type: Date,
+      required: [true, 'Please add starting date'],
+      
+    },
+    endDate: {
+        type: Date,
+        required: [true, 'Please add end date'],
+      },
+    description: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Experience', experienceSchema)
